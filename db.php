bd314e136eb94f019bf498f249ba5285e7e65e60
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-02 16:28:06 +0400

    fix DB creds

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$connect=mysqli_connect("localhost","cn31570_geekshav","cn31570_geekshav","lk-g8tl-nuy");
+$connect=mysqli_connect("localhost","cn31570_geekshav","lk-g8tl-nuy","cn31570_geekshav");
 ?>
