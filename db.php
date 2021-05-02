af4605fc435b38c90dbbf2e80c8ad72ba5528a92
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-02 16:23:11 +0400

    localhot db complite )))

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$connect=mysqli_connect("vh376.timeweb.ru","cn31570_geekshav","cn31570_geekshav","lk-g8tl-nuy");
+$connect=mysqli_connect("localhost","cn31570_geekshav","cn31570_geekshav","lk-g8tl-nuy");
 ?>
