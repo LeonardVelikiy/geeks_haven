e79f4e3863dbaf61d3414260d8f9d10cf4298d42
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-08 18:58:49 +0400

    fix author

--- auhtor/sigin.php
+++ auhtor/sigin.php
@@ -5,22 +5,24 @@ session_start();
 
 <link rel=stylesheet type=text/css href=../style.css>
 <div class="sigin">
-<form method=POST action=test.php>
-		
-		<label>Логин</label>
+
+<?php if(!$_SESSION['user']){
+	echo"<form method=POST action=test.php>
+	<label>Логин</label>
 	<input type=text name=login>
 		<label>Пароль</label>
 	<input type=password name=pass>
 	<input type=submit name=enter></input>
 </form>
-<div class="ilP">
+<div class=ilP>
 	<a>Хотите вернуться?</a><br> 
-	<a href=../index.php style="text-decoration:underline; color: gray;">На главную</a><br>
-	<a>Оставить </a><a href=reg.php style="text-decoration:underline; color: gray;"> учётную запис?</a><br>
-	<a href="?exit=acc" style="text-decoration:underline; color: gray;">Выйти из аккаунта</a>
+	<a href=../index.php style=text-decoration:underline; color: gray;>На главную</a><br>
+	<a>Оставить </a><a href=reg.php style=text-decoration:underline; color: gray;> учётную запис?</a><br>
+	<a href=?exit=acc style=text-decoration:underline; color: gray;>Выйти из аккаунта</a>
 </div>
-</div>
-	
+</div>";
+}
+	?>
 <?php
 $enter=$_POST['enter'];
 	if ($_GET['exit']) 
