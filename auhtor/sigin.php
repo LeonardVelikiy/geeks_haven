40cbd8747d15f85a4e7aa5efe7b613ff3f01cdec
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-08 13:59:11 +0400

    ff

--- auhtor/sigin.php
+++ auhtor/sigin.php
@@ -13,9 +13,9 @@ session_start();
 	<input type=password name=pass>
 	<input type=submit name=enter></input>
 </form>
-<div class="ilP"><!--from Denis  -->
-	<a>Хотите вернуться? </a><br> 
-	<a href=../index.php style="text-decoration:underline; color: gray;">На главную </a><br>
+<div class="ilP">
+	<a>Хотите вернуться?</a><br> 
+	<a href=../index.php style="text-decoration:underline; color: gray;">На главную</a><br>
 	<a>Оставить </a><a href=reg.php style="text-decoration:underline; color: gray;"> учётную запис?</a><br>
 	<a href="?exit=acc" style="text-decoration:underline; color: gray;">Выйти из аккаунта</a>
 </div>
