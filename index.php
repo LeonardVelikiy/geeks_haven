1edeaf1552896db80068f554514edc5faee3e6a2
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-15 20:56:28 +0400

    fix pagination

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ session_start();
 						$int_count++;
 					}
 					for ($i=0; $i <$int_count ; $i++) { 
-						echo"<a href=/?page_number=$i> $p</a>";
+						echo"<a href=/?page_number=$i>  $p</a>";
 						$p++;
 					}
 					/*пагинация*/
