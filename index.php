1737b52d2acc875e538e69be07d9b9c707a0b681
LeonardVelikiy <nik.golovash@bk.ru>
2021-05-15 22:13:43 +0400

    decor for pagination

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ session_start();
 						$int_count++;
 					}
 					for ($i=0; $i <$int_count ; $i++) { 
-						echo"<a href=/?page_number=$i style=font-size:20px;>  	$p</a>";
+						echo"<a href=/?page_number=$i style=font-size:20px; text-decoration:none; color:red;>  	$p</a>";
 						$p++;
 					}
 					/*пагинация*/
