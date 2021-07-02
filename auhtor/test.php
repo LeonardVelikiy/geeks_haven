34f714861f48ab8f73770fd650313d281ed07eb8
Your Name <nik.golovash@bk.ru>
2021-07-02 10:01:22 +0400

    fix decor code

--- auhtor/test.php
+++ auhtor/test.php
@@ -9,8 +9,7 @@
 	$str_auth="SELECT * FROM `players_and_masters` WHERE `login` = '$login' AND `pass`= '$pass'";
 	$run_auth=mysqli_query($connect,$str_auth);
 	$check_users=mysqli_num_rows($run_auth);
-	/*$players=mysqli_fetch_array($run_auth);*/
-	$check_users=mysqli_num_rows($run_auth);
+	
 		if ($check_users) 
 		{
 			$user= mysqli_fetch_assoc($run_auth);
